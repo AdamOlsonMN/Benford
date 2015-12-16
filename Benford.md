24ff9ea3193f2690b65cc3512042ca6a2e7deddf
AdamOlsonMN <olso4075@CLA-SPEYSIDE.ad.umn.edu>
2015-12-16 16:05:42 -0600

    Initial Commit

---
+++ Benford.md
@@ -0,0 +1,33 @@
+---
+layout: post
+title: Benford's Law and 2014 Republican Senate Results
+comments: True
+tags:
+- Academics
+- Real Life
+- Statistics
+- RStats
+---
+
+At dinner last night, I was trying to explain to people that some scholars think you can apply Benford's Law to election returns and deviations
+
+# Benford's Law
+
+Digit   | Frequency
+:------:|:---------:
+alabama | 2
+alaska  | 3
+arizona | 5
+arkansas| 5
+
+Statistic   | Value
+:------:|:---------:
+alabama | 0.5
+alaska  | 1/12 (0.8333...)
+arizona | -1.2
+arkansas| 0
+
+
+# Minnesota Senate Results
+# An R Package for Speedier Analysis
+https://github.com/carloscinelli/benford.analysis
---
+++ benford.R
@@ -0,0 +1,37 @@
+# Quick Bedford Law thing
+library(ggplot2)
+library(plyr)
+
+# Load Data
+SenateReturn <- read.csv("H:/Senate2014.csv", stringsAsFactors=FALSE)
+
+## Drop Precints where leading number is zero
+SenateReturn <- subset(SenateReturn, SenRFirst > 0)
+
+# Create Benford Distro
+(benford=log(1+1/(1:9))/log(10))
+names(benford)=1:9
+
+# Plot Senate Data
+SenatePlot <-ggplot(SenateReturn, aes(x=SenRFirst))
+SenatePlot <- SenatePlot + theme_bw()
+SenatePlot <- SenatePlot + geom_histogram(aes(y = ..density..), binwidth = 1)
+SenatePlot <- SenatePlot + labs(title = "Distribution of All First Digits, Republican Candidate for Senate 2014", x = "First Digits in Precinct")
+
+#Plot Benford Data
+barplot(benford,col="white",ylim=c(-.045,.3))
+abline(h=0)
+title(main="Distribution of First Digits according to Benford's Law", 
+      xlab="Digit", ylab="Proportion of Digits")
+
+# Try the analysis with a package
+## Load Package
+install.packages("benford.analysis")
+library(benford.analysis)
+
+## Run Analysis
+bfd.cp <- benford(SenateReturn$SenR)
+
+## Look at it
+plot(bfd.cp)
+bfd.cp
