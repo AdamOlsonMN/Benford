e40bd1d3f78c982044e8bf1d1db6523bbc0e9c2e
AdamOlsonMN <akomonday@gmail.com>
2015-12-16 19:05:36 -0600

    Just fixing the scale
    Will write it up tomorrow

--- benford.R
+++ benford.R
@@ -1,9 +1,12 @@
 # Quick Bedford Law thing
 library(ggplot2)
 library(plyr)
+require(scales)
 
 # Load Data
 SenateReturn <- read.csv("H:/Senate2014.csv", stringsAsFactors=FALSE)
+SenateReturn <- read.csv("C:/Users/Olson/Desktop/Benford/Senate2014.csv", stringsAsFactors=FALSE)
+
 
 ## Drop Precints where leading number is zero
 SenateReturn <- subset(SenateReturn, SenRFirst > 0)
@@ -13,10 +16,11 @@ SenateReturn <- subset(SenateReturn, SenRFirst > 0)
 names(benford)=1:9
 
 # Plot Senate Data
-SenatePlot <-ggplot(SenateReturn, aes(x=SenRFirst))
+SenatePlot <- ggplot(SenateReturn, aes(x=SenRFirst))
 SenatePlot <- SenatePlot + theme_bw()
-SenatePlot <- SenatePlot + geom_histogram(aes(y = ..density..), binwidth = 1)
+SenatePlot <- SenatePlot + geom_histogram(aes(y = ..density..), binwidth = 1, breaks=seq(1, 10, by=1))
 SenatePlot <- SenatePlot + labs(title = "Distribution of All First Digits, Republican Candidate for Senate 2014", x = "First Digits in Precinct")
+SenatePlot <- SenatePlot + xlim(c(1,9)) + ylim(c(0,.3)) + scale_x_continuous(breaks=pretty_breaks(n=9))
 
 #Plot Benford Data
 barplot(benford,col="white",ylim=c(-.045,.3))
